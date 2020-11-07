064417b808c9c41d226296df878d4c4df45d255c
lacie-life <00sao00ios00@gmail.com>
2020-11-07 19:01:11 +0700

    updated

--- Jetbot/mqtt_example/scripts/mqtt.py
+++ Jetbot/mqtt_example/scripts/mqtt.py
@@ -7,10 +7,11 @@ import sys
 import paho.mqtt.client as mqtt
 import paho.mqtt.subscribe as subscribe
 
+pub = rospy.Publisher('jetbot_cmd', String, queue_size=10)
+rospy.init_node('mqtt')
+rate = rospy.Rate(10) # 10hz
+
 def cmd(msg):
-    pub = rospy.Publisher('jetbot_cmd', String, queue_size=10)
-    rospy.init_node('mqtt')
-    rate = rospy.Rate(10) # 10hz
     pub.publish(String(msg))
 
 def print_msg(client, userdata, message):
@@ -19,9 +20,4 @@ def print_msg(client, userdata, message):
     print(type(msg))
     cmd(msg)
 
-if __name__ == '__main__':
-    try:
-        subscribe.callback(print_msg, "demo/test", hostname="www.citlab.xyz")
-        rospy.spin()
-    except rospy.ROSInterruptException:
-        pass
+subscribe.callback(print_msg, "demo/test", hostname="www.citlab.xyz")
